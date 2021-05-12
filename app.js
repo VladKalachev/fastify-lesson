49fd63d57104dd2faa26dc8d97b92ab926dcfb85
Corvax <vlvete1990@yandex.ru>
2021-05-12 19:45:27 +0300

    chore: added fastify-autoload and routes folder

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 import Fastify from 'fastify';
+import Route from './test/route.js';
+import {dirname, join} from 'path';
+import {fileURLToPath} from 'url';
+import AutoLoad from 'fastify-autoload';
 
 const fastify = Fastify();
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
 
-fastify.register(import('./test/route.js'));
-
-fastify.get('/', async function(request, reply) {
-  return "Hello World";
+fastify.register(AutoLoad, {
+  dir: join(_dirname, 'routes')
 });
+
+fastify.register(Route);
+
+// fastify.get('/', async function(request, reply) {
+//   return "Hello World";
+// });
 
 fastify.listen(3000, () => {
   console.log('running on port 3000');
